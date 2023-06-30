2062047d91666f3528afea3e2104cdbb2db45dbb
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-06-30 21:45:02 +0300

    🛠️refactor(app) add validation to calculateResult function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,6 +30,9 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
+  if(!calculationType === "ADD" || calculationType === "SUBTRACT" || calculationType === "MULTIPLY" || calculationType === "DIVIDE"){
+    return;
+  }
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   let mathOperator;
@@ -47,6 +50,7 @@ function calculateResult(calculationType) {
     currentResult /= enteredNumber;
     mathOperator = "/";
   }
+
   createAndWriteOutput(mathOperator, initialResult, enteredNumber);
   writeToLog(calculationType, initialResult, enteredNumber, currentResult);
 }
